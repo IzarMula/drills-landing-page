7ea8e9d8a199e93613bca355ac03c4859519a2cf
Izar Mula <95627917+IzarMula@users.noreply.github.com>
2024-02-26 21:51:08 +0100

    adding animation

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,13 +2,28 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			keyframes: {
+				'drill-right': {
+					'100% , 0%': { transform: 'translateX(1800px)'},
+					'100%': {transform: 'translateX(0)'}
+				},
+				'drill-left': {
+					'0% , 100%': { transform: 'translateX(-1800px)'},
+					'100%': {transform: 'translateX(0)'}
+				}
+			}
+		},
 		fontFamily: {
 			'brush': ['"Brush Script MT"', "sans-serif"]
 		  },
-		  letterSpacing: {
+		letterSpacing: {
 			widest: '1.1em'
-		  }
+		  },
+		animation: {
+			'drill-right': 'drill-right 1.5s ease-in-out',
+			'drill-left': 'drill-left 3.5s ease-in-out'
+		}
 	},
 	plugins: [],
 }
